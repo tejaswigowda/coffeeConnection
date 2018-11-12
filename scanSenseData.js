66921106f8c8cd43e8b5c2a409d100dc9d58a516
tejaswigowda <tejaswi@foxyninjastudios.com>
2018-11-12 11:45:24 -0700

    ac

---
+++ scanSenseData.js
@@ -0,0 +1,30 @@
+var wifiLocation = require('wifi_location')
+var ssidPrefix = "FDN";
+
+var CronJob = require('cron').CronJob;
+var job = new CronJob({
+  cronTime: '00 */1 * * * *',
+  onTick: function() {
+    console.log("cron");
+    scanNow();
+  },
+  start: false,
+  timeZone: 'America/Phoenix'
+});
+//job.start();
+
+function scanNow(){
+  wifiLocation.wifiTowers(function(err, val) {
+    if(!err){
+      for(var i = 0; i < val.length;i++){
+        if(val[i].ssid.indexOf(ssidPrefix)>=0){
+          var data = val[i].ssid
+          console.log(data);
+        }
+      }
+    }
+  });
+}
+
+//for degugging
+setInterval(scanNow,100);
